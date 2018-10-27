37cb5354d8f1db46c7e23407340d7e6a8da1f376
tc-imba <liuyh615@sjtu.edu.cn>
2018-10-27 21:37:45 +0800

    Split definition and declaration of input parser

--- input_parser/InputParser.h
+++ input_parser/InputParser.h
@@ -21,25 +21,7 @@ public:
         Data() = default;
     };
 
-    static Data parse(std::ifstream &is) {
-        Data data;
-        is >> data.rowNum >> data.colNum >> data.steps >> data.targetScore;
-        data.board.reserve(data.rowNum);
-        for (size_t i = 0; i < data.rowNum; i++) {
-            std::vector<char> row;
-            row.reserve(data.colNum);
-            for (size_t j = 0; j < data.colNum; j++) {
-                int temp = 0;
-                // Set default angle as 0
-                if (!is.eof()) {
-                    is >> temp;
-                }
-                row.emplace_back((char) temp);
-            }
-            data.board.emplace_back(std::move(row));
-        }
-        return data;
-    }
+    static Data parse(std::ifstream &is);
 };
 
 
